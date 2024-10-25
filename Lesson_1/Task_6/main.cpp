71006be78a12661466178fb5e470b6b9e347efcf
Yaroslav Myronenko <jarik2707@gmail.com>
2024-10-26 01:56:13 +0300

    Changed Task 6

--- Lesson_1/Task_6/main.cpp
+++ Lesson_1/Task_6/main.cpp
@@ -1,99 +1,59 @@
 #include <iostream>
-#include <thread>
-#include <chrono>
 #include <cstdlib>
+#include<windows.h>  // бібліотека для команди затримки "Sleep()"
 using namespace std;
 
-// Функція для очищення екрану
-void clearScreen() {
-#ifdef _WIN32
-    system("cls");
-#else
-    system("clear");
-#endif
-}
-
-// Функція для затримки
-void pause(int seconds) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(seconds));
-}
-
-// Функція для малювання трикутника в нормальному положенні
-void TriangleNormal(int height) {
-    for (int i = 1; i <= height; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
-
-// Функція для малювання перевернутого трикутника
-void TriangleUpsideDown(int height) {
-    for (int i = height; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) {
-            cout << "*";
-        }
-        cout << endl;
-    }
-}
-
-// Функція для малювання трикутника, повернутого на 90 градусів
-void TriangleRight(int height) {
-    for (int i = 1; i <= height; i++) {
-        for (int j = 1; j <= height; j++) {
-            if (j < height - i + 1) {
-                cout << " ";
-            } else {
-                cout << "*";
-            }
-        }
-        cout << endl;
-    }
-}
-
-// Функція для малювання трикутника, повернутого на 270 градусів
-void TriangleLeft(int height) {
-    for (int i = height; i >= 1; i--) {
-        for (int j = 1; j <= height; j++) {
-            if (j < height - i + 1) {
-                cout << " ";
-            } else {
-                cout << "*";
-            }
-        }
-        cout << endl;
-    }
-}
-
-int main() {
-    int height;
-    cout << "Enter height:" << endl;
+int main()
+{
+    int height = 0;
+    cout << "Entered height triangle: --> " << endl;
     cin >> height;
-    while (true) {
-        // Малюємо трикутник у нормальному положенні
-        clearScreen();
-        TriangleNormal(height);
-        pause(220);
-
-        // Малюємо трикутник, повернутий на 90 градусів
-        clearScreen();
-        TriangleRight(height);
-        pause(220);
-
-        // Малюємо перевернутий трикутник
-        clearScreen();
-        TriangleUpsideDown(height);
-        pause(220);
-
-        // Малюємо трикутник, повернутий на 270 градусів
-        clearScreen();
-        TriangleLeft(height);
-        pause(220);
+    for (int circle = 0; circle <= 8; circle++) {
+    system("cls"); // Очистка консолі
+    for (int a = 1; a <= height; a++) {   // Трикутник
+        for (int b = 1; b <= a; b++) {
+            cout << "*";
+        }
+        cout << endl;
     }
 
+    Sleep(200);   // Затримка консолі
+    system("cls");
+    for (int a = height; a >= 1; a--) {  // -Трикутник
+        for (int b = 1; b <= a; b++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    Sleep(200);
+    system("cls");
+    for (int a = 1; a <= height; a++) {        //Трикутник перевернутий на 90 градусів
+        for (int b = 1; b <= height; b++) {
+            if (b < height - a + 1)
+                cout << " ";
+            else cout << "*";
+        }
+        cout << endl;
+    }
+
+    Sleep(200);
+    system("cls");
+    for (int a = height; a >= 1; a--) {       //Трикутник повернутий на 270
+        for (int b = 1; b <= height; b++) {
+            if (b < height - a + 1)
+                cout << " ";
+            else cout << "*";
+        }
+        cout << endl;
+    }
+
+    Sleep(200);
+    system("cls");
+    }
     return 0;
 }
+
 
 
 
