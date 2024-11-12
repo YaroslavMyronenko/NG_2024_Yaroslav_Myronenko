b7178db1e49f46a3649c3923140975644bb4dd6d
Yaroslav Myronenko <jarik2707@gmail.com>
2024-11-12 19:28:38 +0200

    Aligned the style of the code

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -7,17 +7,17 @@ int main()
     int stars = 0;
     cout << "Enter amount height:--> ";
     cin >> stars;
-        for ( int height = 0; height < stars; height++ ) {
-          for (int space = 0; space < stars - height - 1; space++ )
-             cout << " ";
-            for ( int width = 0; width < 2 * height + 1; width++ )
-                cout << "*";
-            cout << endl;
-        }
-        int ofset = stars - 1;
-        for (int spase = 0; spase < ofset; spase++ ) {
+    for ( int height = 0; height < stars; height++ ) {
+        for (int space = 0; space < stars - height - 1; space++ )
             cout << " ";
-        }
-        cout << "*" << endl;
-        return 0;
+        for ( int width = 0; width < 2 * height + 1; width++ )
+            cout << "*";
+        cout << endl;
+    }
+    int ofset = stars - 1;
+    for (int spase = 0; spase < ofset; spase++ ){
+        cout << " ";
+    }
+    cout << "*" << endl;
+    return 0;
 }
