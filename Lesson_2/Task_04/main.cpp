f277e6062cbac675366e67da7cfa9e0bb20a0d04
Yaroslav Myronenko <jarik2707@gmail.com>
2024-11-19 21:22:29 +0200

    Modified2 change string on char

--- Lesson_2/Task_04/main.cpp
+++ Lesson_2/Task_04/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-    int char_count[256] = {0};
-    string Line;
+    unsigned short int char_count[256] = {0};
+    char Line[256];
     cout << "Enter Line: --> ";
-    getline (cin, Line);
-    for (int counter = 0; counter < Line.size(); counter++){
+    cin.getline(Line, 256);
+    unsigned short int length = strlen(Line);
+    for (int counter = 0; counter < length; counter++){
         char_count[(unsigned char)Line[counter]]++;}
 
     for (int counter = 0; counter < 256; counter++){
